e2aec597c452f29f5d9d7b2edf46a4f10ce9e0df
k-calderon <bluelotus79@gmail.com>
2019-03-03 15:09:50 -0800

    Cleaned up and reformatted files. Removed deprecated code

--- js/sample-table.js
+++ js/sample-table.js
@@ -1,47 +1,37 @@
 const SAMPLESIMPLETABLE = {
-    "meta":{
-        "name":"Sample Simple Table",
-        "id":"sampleTable",
-        "type":"simple",
-        "rangeMin": 1,
-        "rangeMax": 20
-    },
-    "table": [
-        "Result 1",
-        "Result 2",
-        "Result 3",
-        "Result 4"        
-    ]
+  meta: {
+    name: "Sample Simple Table",
+    id: "sampleTable",
+    type: "simple",
+    rangeMin: 1,
+    rangeMax: 20
+  },
+  table: ["Result 1", "Result 2", "Result 3", "Result 4"]
 };
 
 const SAMPLERANGETABLE = {
-    "meta": {
-        "name": "Sample Range Table",
-        "id":"sample-range-table",
-        "type":"simple",
-        "rangeMin": 1,
-        "rangeMax": 20
-    },
-    "table": [
-        [1, 5, "Result 1"],
-        [6, 10, "Result 2"],
-        [11, 15, "Result 3"],
-        [16, 20, "Result 4"]
-    ]
+  meta: {
+    name: "Sample Range Table",
+    id: "sample-range-table",
+    type: "simple",
+    rangeMin: 1,
+    rangeMax: 20
+  },
+  table: [
+    [1, 5, "Result 1"],
+    [6, 10, "Result 2"],
+    [11, 15, "Result 3"],
+    [16, 20, "Result 4"]
+  ]
 };
 
 const SAMPLEPROBABILITYTABLE = {
-    "meta": {
-        "name": "Sample Range Table",
-        "id":"sample-probabiliy-table",
-        "type":"simple",
-        "rangeMin": 1,
-        "rangeMax": 20
-    },
-    "table": [
-        [5, "Result 1"],
-        [5, "Result 2"],
-        [5, "Result 3"],
-        [5, "Result 4"]
-    ]
+  meta: {
+    name: "Sample Range Table",
+    id: "sample-probabiliy-table",
+    type: "simple",
+    rangeMin: 1,
+    rangeMax: 20
+  },
+  table: [[5, "Result 1"], [5, "Result 2"], [5, "Result 3"], [5, "Result 4"]]
 };
