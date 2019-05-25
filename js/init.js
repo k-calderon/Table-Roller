1dea944be850594b68dfccb6c146d00c60007a20
k-calderon <bluelotus79@gmail.com>
2019-05-25 08:40:50 -0700

    Setup test button

--- js/init.js
+++ js/init.js
@@ -5,6 +5,12 @@ const log = loggers.log;
 const init = {
   testString: "testing",
   testFunction: () => log("test function fired"),
+  button: function (){
+    document.getElementById("rollButton").addEventListener("click", function(){
+      let result = roll.simpleTable(SAMPLESIMPLETABLE);
+      document.getElementById("target").innerHTML = result;
+    })
+  },
   all: function() {
     for (let key in init) {
       if (
